7ace8cd8adf7b3763fe1ab37e5b095a854e53f6c
changchen <doublechang123@163.com>
2019-08-06 13:04:05 +0800

    modify AppUtils

--- gulimall-core/src/main/java/com/atguigu/gulimall/commons/utils/AppUtils.java
+++ gulimall-core/src/main/java/com/atguigu/gulimall/commons/utils/AppUtils.java
@@ -8,14 +8,14 @@ public class AppUtils {
     public static String arrayToString(String[] array,String spe){
 
         StringBuffer stringBuffer = new StringBuffer();
+        String str = "";
         if (array != null){
             for (String s : array) {
                 stringBuffer.append(s);
                 stringBuffer.append(spe);
             }
+            str = stringBuffer.toString().substring(0,stringBuffer.length()-1);
         }
-
-        String str = stringBuffer.toString().substring(0,stringBuffer.length()-1);
 
         return str;
     }
